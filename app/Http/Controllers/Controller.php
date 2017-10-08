d1d340fb89982553f8d19484c832c7802e8a72c8
bishalbaaniya123 <071bct513@pcampus.edu.np>
2017-10-08 20:25:50 +0545

    setup laravel and create required files in controller and models

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,71 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
 
-class Controller extends BaseController
+class UserController extends Controller
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    /**
+     * constructor for user controller
+     */
+    public function __contruct()
+    {
+    }
+
+    /**
+     * logs in user with provided credentials
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function login(Request $request)
+    {
+    }
+
+    /**
+     * clears user session upon request
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function logout(Request $request)
+    {
+    }
+
+    /**
+     * register new user account to the system
+     *
+     * @param Request $request
+     */
+    public function register(Request $request)
+    {
+    }
+
+    /**
+     * reset user's forgotten password following required procedure/link
+     *
+     * @param Request $request
+     */
+    public function resetPassword(Request $request)
+    {
+    }
+
+    /**
+     * helps user to change their account's password
+     *
+     * @param Request $request
+     */
+    public function changePassword(Request $request)
+    {
+    }
+
+    /**
+     * generates forgotten password reset link/procedure and sends it to the
+     * user through some medium (mail, sms or something else)
+     *
+     * @param Request $request
+     */
+    public function requestResetPassword(Request $request)
+    {
+    }
 }
